76c10dee500c8f9d7bc7fef541db7c9e97d084eb
Jollen Chen <jollen@moko365.com>
2016-10-01 08:09:22 +0800

    Add wwwRPC API spec

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports = RPC
+
+/**
+ * Represents a RPC operation.
+ * @constructor
+ * @param {string} from - The URI of sender.
+ * @param {string} to - The URI of receiver.
+ */
+function RPC(from, to) {
+}
+
+/**
+ * To send a RPC operation.
+ * @constructor
+ * @param {object} message - The message to be sent.
+ */
+RPC.prototype.send = function(message) {
+
+}
+
+/**
+ * To serialize a message.
+ * @param {Object} message - The message to be serialized.
+ * @returns {String} The document (string) of message (JSON object).
+ */
+RPC.prototype.serialize = function(message) {
+
+}
+
+/**
+ * To deserialize a message.
+ * @param {String} document - The document to be deserialized.
+ * @returns {Object} JSON object of the document.
+ */
+RPC.prototype.deserialize = function(document) {
+
+}
